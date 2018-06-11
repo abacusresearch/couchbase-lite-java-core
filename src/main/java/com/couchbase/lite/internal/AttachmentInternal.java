397463044fe13374491998286fb9688aa04c608c
Remo Inverardi <remo.inverardi@abacus.ch>
2018-06-11 10:52:17 +0200

    Fix the 'missing_stub' errors.
    See https://github.com/couchbase/couchbase-lite-android/issues/994

--- src/main/java/com/couchbase/lite/internal/AttachmentInternal.java
+++ src/main/java/com/couchbase/lite/internal/AttachmentInternal.java
@@ -126,6 +126,18 @@ public class AttachmentInternal {
             // I can't handle this myself; my caller will look it up from the getDigest
             if (digest == null)
                 throw new CouchbaseLiteException(Status.BAD_ATTACHMENT);
+
+            // @remo Upstream!
+            //
+            // See https://github.com/couchbase/couchbase-lite-android/issues/994
+
+            if (attachInfo.containsKey("revpos")) {
+                int revPos = ((Number) attachInfo.get("revpos")).intValue();
+                if (revPos <= 0) {
+                    throw new CouchbaseLiteException(Status.BAD_ATTACHMENT);
+                }
+                setRevpos(revPos);
+            }
         } else {
             throw new CouchbaseLiteException(Status.BAD_ATTACHMENT);
         }
